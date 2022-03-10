0b64410dc2b7cda73e5c596a8ed3f6bbc5f7f5ad
Magnus Ljung <ljung.magnus@gmail.com>
2022-03-10 18:47:24 +0100

    startar igång server o kopplar upp mot databasen

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,16 @@ const express = require('express');
 //hämtar hem Router metoden från express så att vi kan använda oss av GET, PUT, POST, PATCH  
 const router = express.Router();
 
+//hämta alla produkter från databasen med hjälp av en GET
+
+//hämta en enskild produkt med en GET
+
+//lägga till en produkt med en POST
+
+//uppdatera en produkt med en PUT
+
+//ta bort en produkt med DELETE
+
 
 module.exports = router;
 
--- backend/models/products/productSchema.js
+++ backend/models/products/productSchema.js
@@ -1,5 +1,6 @@
+//hämtar hem mongoose som hjälper oss skapa ett Schema
 const mongoose = require('mongoose');
-
+//
 const productSchema = mongoose.Schema({
 
   name:     { type: String, required: true, unique: true },
@@ -9,5 +10,5 @@ const productSchema = mongoose.Schema({
 
 }, { timestamps: true })
 
-
+//exporterar modellen och ger min collection namnet "Product(s)"
 module.exports = mongoose.model('Product', productSchema)
